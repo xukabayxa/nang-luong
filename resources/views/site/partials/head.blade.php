53d03496c4d941807bbc1f41a455cfa25b99d3e3
dnsnamdang <namdangit@gmail.com>
2022-05-11 07:13:04 +0700

    update..

--- resources/views/site/partials/head.blade.php
+++ resources/views/site/partials/head.blade.php
@@ -19,7 +19,8 @@
     <link href="/plugins/fontawesome/css/all.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+
     <!-- / Yoast SEO plugin. -->
 
 
